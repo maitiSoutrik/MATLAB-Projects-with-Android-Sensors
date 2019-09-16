90558619c01f0a3331f259f91715f7c2d9c528c4
Soutrik Maiti <44332983+iAmDocBoom@users.noreply.github.com>
2019-09-16 15:06:50 -0500

    Files uploades

---
+++ Shaking.m
@@ -0,0 +1,45 @@
+% Shaking
+% Detects whether phone is shaking
+%  
+% Copyright 2016 MatrixUniversity.net
+
+% Enable Connector
+connector on
+
+% Connect to device
+m = mobiledev;
+
+% Enable Sensor and Start Logging
+m.AngularVelocitySensorEnabled=1;
+m.Logging=1;
+
+% Run a loop, checking for orientation
+for k = 1:200
+    % Acquire 1 second of data
+    pause(1)
+    [av,t] = angvellog(m);
+    discardlogs(m)
+    
+    if ~isempty(av)
+        
+        avm = mean(av);
+        % Change vectors to scalars
+        % Removes dependency on Orientation
+        x = avm(:,1);
+        y = avm(:,2);
+        z = avm(:,3);
+        mag = sqrt(sum(x.^2 + y.^2 + z.^2, 2));
+        
+        % Check Orientation and Display Results
+        if mag > 1
+            disp('Shaking')
+        else
+            disp('Idle')
+        end
+    end
+end
+
+% Clean up
+clear m
+connector off
+
